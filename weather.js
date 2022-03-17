6e566d94759d6e2178ae293ad740e1b25e6b3431
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2022-03-17 14:35:32 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,43 @@
+let weather = {
+    "apiKey" : "263c997de01a38c427a765728fdaac5f",
+    fetchWeather: function (city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city + "&units=metric&appid=" + this.apiKey).
+        then((response)=> response.json()).then((data)=> this.displayWeather(data));
+    },
+    displayWeather : function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        const {dt, timezone} = data;
+        const dandt = new Date(dt * 1000 + timezone * 1000);
+        const dtString = dandt.toDateString();
+        console.log(name,icon, description,temp,humidity,speed, dtString);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp+" °C";
+        document.querySelector(".humidity").innerText = "Humidity : "+humidity+"%";
+        document.querySelector(".windspeed").innerText = "Wind speed : "+speed+" kmph";
+        document.querySelector(".date").innerText = dtString;
+        document.body.style.backgroundImage= "url('https://source.unsplash.com/1920x1080/?"+name+"')";
+    },
+    search : function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search").addEventListener("click", function(){
+weather.search();
+});
+
+document.querySelector(".search").addEventListener("keyup", function(event){
+if(event.key=="Enter"){
+    weather.search();
+}
+});
+
+weather.fetchWeather("Kolkata");
+
+// url = https://api.openweathermap.org/data/2.5/find?q=Kolkata&units=metric&appid=263c997de01a38c427a765728fdaac5f
