3f9c35c0c5371d0856ea55dfbf8d3580d68880d1
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2023-06-29 19:55:35 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -21,7 +21,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity : "+humidity+"%";
         document.querySelector(".windspeed").innerText = "Wind speed : "+speed+" kmph";
         document.querySelector(".date").innerText = dtString;
-        document.body.style.backgroundImage= "url('https://unsplash.com/collections/1408037/weather')";
+        document.body.style.backgroundImage= "url('https://unsplash.com/photos/RC6xanaNCJY')";
     },
     search : function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
